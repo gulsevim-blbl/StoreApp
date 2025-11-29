bfc4e2a83d523265e5c60be0ea3fc8229a6f3ca4
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-11-30 00:39:12 +0300

    Routing

--- StoreApp/StoreApp.Web/obj/Debug/net8.0/StoreApp.Web.AssemblyInfo.cs
+++ StoreApp/StoreApp.Web/obj/Debug/net8.0/StoreApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989c1f466d308da0c9d6c84f02ff6eae5c994639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f265fcba07439eb9082c21f332e9c1883e12ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
