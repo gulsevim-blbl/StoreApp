774e993e1b90feb727975fcfd3ab1f11e9b637ec
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-11-30 17:02:40 +0300

    AutoMapper

--- StoreApp/StoreApp.Web/Models/ProductViewModel.cs
+++ StoreApp/StoreApp.Web/Models/ProductViewModel.cs
@@ -6,7 +6,8 @@ public class ProductViewModel
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
-    public string Category { get; set; } = string.Empty;
+
+    // public string Category { get; set; } = string.Empty; //burayı siliyoruz.  
 }
 public class ProductListViewModel
 {
